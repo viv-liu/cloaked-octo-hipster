243e270fe8509d55e5e2978708c0e68955bd23e6
clamchowder <lamchester@gmail.com>
2015-08-28 20:34:26 -0700

    Backend changes for cook time and difficulty
    Have RecipeLookupTask populate the time/difficulty fields in RecipeItem

--- app/src/main/java/com/example/android/foodstorm/RecipeItem.java
+++ app/src/main/java/com/example/android/foodstorm/RecipeItem.java
@@ -18,24 +18,31 @@ public class RecipeItem {
 	public int image;               // preferred over imageDrawable (used for testing)
 	//public Bitmap imageBitmap;         // more convenient for getting images off web
 	public String imageUrl;
+	public int minutes;
+	public int difficulty;
 	List<RecipeDirection> directions;
 	List<FoodItem> ingredients;
 	public int id;
-	
+
 	public RecipeItem(String t, String desc){
 		title = t;
 		description = desc;
 		image = -1;
 		imageUrl = null;
+		minutes = 1;
+		difficulty = 0;
 		directions = new ArrayList<RecipeDirection>();
+		ingredients = new ArrayList<FoodItem>();
 	}
-	
-	public RecipeItem(String t, String desc, int i, List<RecipeDirection> d, List<FoodItem> ing) {
+
+	public RecipeItem(String t, String desc, int i, int m, int dif, List<RecipeDirection> dir, List<FoodItem> ing) {
 		title = t;
 		description = desc;
 		image = i;
 		imageUrl = null;
-		directions = d;
+		minutes = m;
+		difficulty = dif;
+		directions = dir;
 		ingredients = ing;
 	}
 }
